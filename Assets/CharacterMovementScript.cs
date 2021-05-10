689b9fb71f1116e1eb1f5543c04c4aea808a7250
Alex <kiel181@wp.pl>
2021-05-10 14:17:44 +0100

    basic player movement

--- Assets/CharacterMovementScript.cs
+++ Assets/CharacterMovementScript.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class CharacterMovementScript : MonoBehaviour
 {
     public CharacterController controller;
+    public Camera camera;
     public Joystick leftJoystick, rightJoystick;
     private Vector3 vector3;
     public float speed = 3f;
+    public float sensitivity = 2f;
 
-    private float move_x, move_z;
+    private float move_x, move_z, rot_x, rot_y;
 
 
     // Start is called before the first frame update
@@ -23,7 +25,16 @@ public class CharacterMovementScript : MonoBehaviour
     {
         move_x = leftJoystick.Horizontal * speed;
         move_z = leftJoystick.Vertical * speed;
+
+        rot_x = rightJoystick.Horizontal * sensitivity;
+        rot_y = rightJoystick.Vertical * sensitivity;
+
         vector3 = new Vector3(move_x, 0, move_z);
+
+        transform.Rotate(0, rot_x, 0);
+        camera.transform.Rotate(-rot_y, 0, 0);
+        vector3 = transform.rotation * vector3;
+
         controller.Move(vector3 * Time.deltaTime);
     }
 }
