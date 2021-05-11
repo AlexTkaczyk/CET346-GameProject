7e71fe9da2792ff88330536c3e26b09962f870ae
Alex <kiel181@wp.pl>
2021-05-11 15:56:04 +0100

    Character controller fixes
    Fixed an issue with floating character controller by adding gravity to the script.

--- Assets/MyScripts/CharacterMovementScript.cs
+++ Assets/MyScripts/CharacterMovementScript.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class CharacterMovementScript : MonoBehaviour
 {
     public CharacterController controller;
-    public Camera camera;
     public Joystick leftJoystick;
     private Vector3 vector3;
     public float speed = 3f;
     public float sensitivity = 1f;
     private float rot_x, move_z;
+    private float gravity = 14f;
+    private float verticalVelocity;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -23,9 +25,18 @@ public class CharacterMovementScript : MonoBehaviour
     {
         move_z = leftJoystick.Vertical * speed;
         rot_x = leftJoystick.Horizontal * sensitivity;
-        transform.Rotate(0, rot_x, 0);
+        transform.Rotate(0, rot_x, 0);  
 
-        vector3 = new Vector3(rot_x, 0, move_z);
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        
+        vector3 = new Vector3(rot_x, verticalVelocity, move_z);
         vector3 = transform.rotation * vector3;
         controller.Move(vector3 * Time.deltaTime);
     }
