379ebe88b91bf8374d01b11ac3088a5ff4c4f155
Alex <kiel181@wp.pl>
2021-05-12 16:38:13 +0100

    NavMesh testing

---
+++ Assets/AI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    //public GameObject sphere, point2;
+    public NavMeshAgent agent;
+    public float delta = 1.5f;
+    public float speed = 5.0f;
+    private Vector3 startPos;
+    public Vector3 point1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = startPos;
+        agent.SetDestination(point1);
+        //v.x += delta * Mathf.Sin(Time.time * speed);
+        //transform.position = v;
+    }
+}
