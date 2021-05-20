3da25d7d1acedb3eac315a874158c94a78f77c83
Alex <kiel181@wp.pl>
2021-05-20 15:26:43 +0100

    AI Random movement

--- Assets/MyScripts/AI.cs
+++ Assets/MyScripts/AI.cs
@@ -8,27 +8,54 @@ public class AI : MonoBehaviour
     //public GameObject player; //sphere, point2;
     public CharacterController controller;
     public NavMeshAgent agent;
-    public float delta = 1.5f;
     public float speed = 5.0f;
-    private Vector3 startPos;
-    public Vector3 point1;
+    public Vector3 point1;// point2;
+
+    public float time;
+    bool isWorking;
 
     // Start is called before the first frame update
     void Start()
     {
-        startPos = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 v = startPos;
-        if(Vector3.Distance(controller.transform.position, agent.transform.position) < 3)
+        if(!isWorking)
         {
-            agent.SetDestination(point1);
+            StartCoroutine(Coroutine());
         }
 
         //v.x += delta * Mathf.Sin(Time.time * speed);
         //transform.position = v;
+    }
+
+    Vector3 getRandomPos()
+    {
+        float x = Random.Range(-10, 10);
+        float z = Random.Range(-20, 20);
+
+        Vector3 pos = new Vector3(x, 0, z);
+        return pos;
+    }
+
+    IEnumerator Coroutine()
+    {
+        isWorking = true;
+        yield return new WaitForSeconds(time);
+        GetNewDestination();
+        isWorking = false;
+    }
+
+    void GetNewDestination()
+    {
+        agent.SetDestination(getRandomPos());
+
+        if (Vector3.Distance(controller.transform.position, agent.transform.position) < 5)
+        {
+            agent.SetDestination(point1);
+        }
     }
 }
